4cbd9fb7c692304e3286ef186364b77588c099f9
Hack Reactor Student <student@hackreactor.com>
2018-09-17 11:51:36 -0700

    implemented getElementsByClassName using closure

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,55 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+/*
+  potential pseudocode for getElementsByClassName without closure
+  Used closure: 
+    declare array inside function:
+      The final solution will recurse thru function
+
+  1. add parameter startNode
+  2. if(startNode === undefined) {
+      startNode = document.body
+    };
+      var matchinNodes = []; //once func runs thru array it'll stop? 
+      if(startNode.hasClassName('className')) {
+          matchingNodes = matchNode.concat(getElementsByClassName(className, ))
+        }
+
+*/
+
+/* pseudocode for getElementsByClassName with closure
+
+  var matchingNodes = [];
+  var body = document.body
+  function traverseTree (startNode) {
+    First Item: 
+      if(startNode.hasClass(className)) {
+        matchingNodes.push(startNode);
+      };
+    iterate through all children of startNode 
+      traverseTree(each body child); 
+    };
+  
+  traverseTree(body);
+  return matchingNodes; 
+  
+
+
+*/
+var getElementsByClassName = function(className) {
+  var matchingNodes = [];
+  var body = document.body;
+  function traverseTree (startNode) {
+      if(startNode.classList && startNode.classList.contains(className)) { 
+        matchingNodes.push(startNode); 
+      };
+    //iterate through all children of startNode
+    for(var i = 0; i < startNode.childNodes.length; i++) {
+      traverseTree(startNode.childNodes[i]);
+      };   
+    };
+  traverseTree(body);
+  return matchingNodes; 
 };
