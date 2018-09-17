ccfab05b31c7ffc8747e918b6b1d9833368e92a8
Hack Reactor Student <student@hackreactor.com>
2018-09-17 12:23:26 -0700

    stringifyJSON passing primitive value tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var stringified = ""
+
+// if obj is a primitive value (e.g. a number, null, boolean, string.)
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+    return stringified + obj;
+  }
+
+  if (typeof obj === 'string') {
+    return '"' +obj+ '"';
+  }
+// if obj is an array
+  // iterate through array
+    // invoke stringifyJSON on each elt of array
+
+// if obj is a function
+  // return undefined
+
+// if obj is an object
+  // iterate through each key, value pair
+    // if 
+  // 
+
 };
+
+console.log(stringifyJSON(9)  === JSON.stringify(9));
+console.log(stringifyJSON(null) === JSON.stringify(null));
+console.log(stringifyJSON(true) === JSON.stringify(true));
+console.log(stringifyJSON(false) === JSON.stringify(false));
+console.log(stringifyJSON('Hello world') === JSON.stringify('Hello world'));
